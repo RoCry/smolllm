90458d8b5df65e98b48ad2f572de39f4a671e190
RoCry <crysheen@gmail.com>
2025-02-24 21:20:24 +0800

    chore: bump version to 0.1.9

--- src/smolllm/__init__.py
+++ src/smolllm/__init__.py
@@ -5,6 +5,6 @@ smolllm - A minimal LLM library for easy interaction with various LLM providers
 from .core import ask_llm
 from .types import LLMFunction, StreamHandler
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __all__ = ["ask_llm", "LLMFunction", "StreamHandler"]
 
