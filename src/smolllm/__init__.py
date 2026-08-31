f87928c91de873a84ad19ff88ef2bf4b70c80eeb
RoCry <crysheen@gmail.com>
2026-08-31 23:10:54 +0800

    chore: bump version to 0.11.3

--- src/smolllm/__init__.py
+++ src/smolllm/__init__.py
@@ -27,7 +27,7 @@ from .types import StreamHandler as StreamHandler
 from .types import StreamResponse as StreamResponse
 from .types import Usage as Usage
 
-__version__ = "0.11.2"
+__version__ = "0.11.3"
 _LAZY_EXPORTS = {
     "ask_llm": ".core",
     "stream_llm": ".core",
